676524e9b7ef1ba838322a6e3c86fcb38e7a5e9f
github username <github email address>
2020-09-05 10:56:36 -0500

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,7 +1,23 @@
+require 'open-uri'
+require 'json'
+require 'net/http'
+require 'pry'
+class GetRequester 
 
+    attr_accessor :url
 
-class GetRequest
-  
- URL = "url"
-  
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+       JSON.parse(get_response_body)
+    end
+
 end
