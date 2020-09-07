50b35517bb14b86ed5ebf4e36d3cf4125eed129d
Kelin Field <kfield111@hotmail.com>
2020-09-07 01:25:01 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,21 @@
-# Write your code here
+require 'net/https'
+require 'open-uri'
+require 'json'
+
+
+class GetRequester
+
+  def initialize (url_address)
+    @url_address = url_address
+  end
+
+  def get_response_body
+    uri = URI.parse(@url_address)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    request = JSON.parse(self.get_response_body)
+  end
+end
